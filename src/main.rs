1b4c097d8bfb4b1c8a77c797ebef99b5f5e786c5
Vi <vi.kitten@proton.me>
2024-01-31 18:27:43 +0000

    file IO done but better :3

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::fs;
 use std::io::Read;
+use std::io::Write;
 use std::path::Path;
 use std::fs::File;
 use serde_json::json;
@@ -8,34 +10,72 @@ use serde_json::Value;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let path = if let Some(p) = args.get(1) {
-        Path::new(p)
-    } else {
-        eprintln!("please provide a file path!");
+    let Some(path_str) = args.get(1) else {
+        eprintln!("please provide a file path");
         return
     };
+    let mut tmp_path_str = path_str.clone();
+    tmp_path_str.push_str(".tmp");
 
-    let mut file = match File::open(path) {
+    let path = Path::new(path_str);
+    let tmp_path = Path::new(&tmp_path_str);
+
+    let mut data: Value = if path.exists() {
+        let mut file = match File::options().read(true).open(path) {
+            Ok(f) => f,
+            Err(error) => {
+                eprintln!("couldn't open {}: {}", path.display(), error);
+                return
+            },
+        };
+    
+        let mut content = String::new();
+        if let Err(error) = file.read_to_string(&mut content) {
+            eprintln!("couldn't read {}: {}", path.display(), error);
+            return
+        }
+    
+        match serde_json::from_str(&content) {
+            Ok(d) => d,
+            Err(error) => {
+                eprintln!("serde_json parse error: {}", error);
+                return
+            }
+        }
+    } else {
+        json!({
+            "n": 0,
+            "name": "Violet"
+        })
+    };
+
+    println!("{}", data["n"]);
+
+    data["n"] = json!(data["n"].as_u64().expect("data.n should be int") + 1);
+
+    let mut tmp_file = match File::options().write(true).create_new(true).open(tmp_path) {
         Ok(f) => f,
-        Err(why) => {
-            eprintln!("couldn't open {}: {}", path.display(), why);
+        Err(error) => {
+            eprintln!("couldn't open {}: {}", path.display(), error);
             return
         },
     };
 
-    let mut content = String::new();
-    if let Err(why) = file.read_to_string(&mut content) {
-        eprintln!("couldn't read {}: {}", path.display(), why);
+    if let Err(error) = tmp_file.write_all(serde_json::to_string(&data).unwrap().as_bytes()) {
+        eprintln!("{}", error);
+        drop(tmp_file);
+        if let Err(error) = fs::remove_file(tmp_path_str) {
+            eprintln!("failure to cleanup: {}", error)
+        };
         return
     }
 
-    let data: Value = match serde_json::from_str(&content) {
-        Ok(d) => d,
-        Err(error) => {
-            eprintln!("serde_json parse error: {}", error);
-            return
-        }
+    if let Err(error) = std::fs::rename(&tmp_path, &path) {
+        eprintln!("{}", error);
+        drop(tmp_file);
+        if let Err(error) = fs::remove_file(tmp_path_str) {
+            eprintln!("failure to cleanup: {}", error)
+        };
+        return
     };
-
-    println!("{}", data);
 }
